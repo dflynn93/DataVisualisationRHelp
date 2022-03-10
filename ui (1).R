1d6e34b0c0551a6424dbf8ef7f87a4e3ec2c73c6
dflynn93 <74905665+dflynn93@users.noreply.github.com>
2022-03-10 10:24:59 +0000

    Update ui (1).R

--- ui (1).R
+++ ui (1).R
@@ -1,4 +1,4 @@
-# Load libraries
+## Load libraries
 library(shiny)
 library(tidyverse)
 
@@ -7,38 +7,40 @@ shinyUI(
   fluidPage(
     br(),
     # TASK 1: Application title
-    ...,
+    titlePanel("Final Project - Week 4"),
     p("Explore the difference between people who earn less than 50K and more than 50K. You can filter the data by country, then explore various demogrphic information."),
     
     # TASK 2: Add first fluidRow to select input for country
     fluidRow(
-      column(12, 
-             wellPanel(...)  # add select input 
-             )
+      column(width = 12, 
+             wellPanel(selectInput(inputId = "country", label = "Country:", choices = c("United-States", "Canada", "Mexico", "Germany", "Philippines")))  # add select input 
+      )
     ),
     
     # TASK 3: Add second fluidRow to control how to plot the continuous variables
     fluidRow(
-      column(3, 
+      column(width = 3, 
              wellPanel(
-               p("Select a continuous variable and graph type (histogram or boxplot) to view on the right."),
-               ...,   # add radio buttons for continuous variables
-               ...    # add radio buttons for chart type
-               )
-             ),
-      column(9, ...)  # add plot output
+               p(radioButtons(inputId = "continuous_variable", label = "Age/Hours:", choices = c("age", "hours_per_week"))),
+               (radioButtons(inputId = "graph_type", label = "Hist/Box:", choices = c("histogram", "box_plot"))
+              )
+      ),
+      column(width = 9, plotOutput('p1'),)  # add plot output
     ),
     
     # TASK 4: Add third fluidRow to control how to plot the categorical variables
     fluidRow(
-      column(3, 
+      column(width = 3, 
              wellPanel(
-               p("Select a categorical variable to view bar chart on the right. Use the check box to view a stacked bar chart to combine the income levels into one graph. "),
-               ...,    # add radio buttons for categorical variables
-               ...     # add check box input for stacked bar chart option
-               )
-             ),
-      column(9, ...)  # add plot output
+               p(radioButtons(inputId = "categorical_variable", label = "categorical:", choices = c("education", "workclass", "sex")),
+                # "Select a categorical variable to view bar chart on the right. Use the check box to view a stacked bar chart to combine the income levels into one graph. "),
+               (checkboxInput(inputId = "is_staked", label = "Stack Bars:", choices = c(FALSE))),    # add radio buttons for categorical variables
+                   # add check box input for stacked bar chart option
+             )
+      ),
+      column(width = 9, plotOutput('p2'))  # add plot output
     )
-  )
-)
+    ))))
+shinyServer(function(input, output) {
+  
+})
